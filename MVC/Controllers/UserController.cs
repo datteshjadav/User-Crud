a35b3e0f9ccfde1a1a1f0cfab7895ef30e00f0e2
Priyanshi Patel <priyanshi2820@gmail.com>
2024-02-22 16:05:59 +0530

    Index() Comment

--- MVC/Controllers/UserController.cs
+++ MVC/Controllers/UserController.cs
@@ -18,10 +18,10 @@ namespace MVC.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+        // public IActionResult Index()
+        // {
+        //     return View();
+        // }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
