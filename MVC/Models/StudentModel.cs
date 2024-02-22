7b5f76596d65c71bf6e20f38954c1672bb525f4b
JeminMolesalam <jeminmolesalam23@gmail.com>
2024-02-23 00:10:03 +0530

    Updated Student Model

--- MVC/Models/StudentModel.cs
+++ MVC/Models/StudentModel.cs
@@ -8,10 +8,10 @@ namespace MVC.Models
     public class StudentModel
     {
         public int c_studid {get;set;} = 0; 
-        public string? c_studname {get;set;} = string.Empty;
+        public string c_studname {get;set;} = string.Empty;
         public int c_studage {get;set;} = 0;
-        public int c_studphone {get;set;} = 0;
-        public string[]? c_studcourse {get;set;} = new string[0];
-        public string? c_studaddress {get;set;} = string.Empty;
+        public long c_studphone {get;set;} = 0;
+        public string c_studcourse {get;set;} = string.Empty;
+        public string c_studaddress {get;set;} = string.Empty;
     }
 }
