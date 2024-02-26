05ed3ea6fb2323d3295cd993072c2df365bb486e
Dev1606 <devmevawala96@gmail.com>
2024-02-26 10:46:46 +0530

    Adjustment for Register merge is done

--- MVC/Controllers/StudentController.cs
+++ MVC/Controllers/StudentController.cs
@@ -8,19 +8,27 @@ using Microsoft.Extensions.Logging;
 
 namespace MVC.Controllers
 {
-    [Route("[controller]")]
+    // [Route("[controller]")]
     public class StudentController : Controller
     {
         private readonly ILogger<StudentController> _logger;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        
 
-        public StudentController(ILogger<StudentController> logger)
+        public StudentController(ILogger<StudentController> logger, IHttpContextAccessor httpContextAccessor)
         {
             _logger = logger;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var session = _httpContextAccessor.HttpContext.Session;
+            if (session.GetInt32("userid") != null){
+                return View();
+            }else{
+                return RedirectToAction("Login", "User");
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
